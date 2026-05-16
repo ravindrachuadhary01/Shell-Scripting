4639c9be43813b91d244e924ce4ff7cb95591a06
Ravindra singh <ravindrafaujdar7284@gmail.com>
2026-05-16 15:40:10 +0530

    update all

--- 01_script.sh
+++ 01_script.sh
@@ -2,8 +2,3 @@
 # This script prints "Hello, World!" to the console.
 
 echo "Hello, World!!!!!!!" 
-
-<<< cmt
-This script is a simple bash script that prints "Hello, World!" to the console. It uses the echo command to display the message. The script can be executed in a terminal or command prompt by running the command `bash 01_script.sh` (assuming the script is saved as 01_script.sh).
-this is a basic example of a shell script, and it can be modified to include more complex functionality as needed.
-cmt
